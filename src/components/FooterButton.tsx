0be083df71f0efe4cd4c8deb64e8cf99dbd4c6e3
Yuma Morimoto <kanten520212@gmail.com>
2025-02-21 02:18:23 +0900

    update: FooterButton実装

--- src/components/FooterButton.tsx
+++ src/components/FooterButton.tsx
@@ -1,26 +1,46 @@
+import { Button, FileButton } from '@mantine/core';
 import { IconPhotoFilled } from '@tabler/icons-react';
-import { ChangeEventHandler } from 'react';
+import { FormEventHandler } from 'react';
 
 type Props = {
   type: 1 | 2; // 1: 画像入力, 2: 食材決定
   label: string;
-  onChange: ChangeEventHandler<HTMLInputElement>;
+  onClick?: FormEventHandler<HTMLButtonElement>;
+  onChange?: (payload: File | null) => void;
 };
 
-export const FooterButton = ({ type, onChange }: Props) => {
+export const FooterButton = ({ type, onClick, onChange }: Props) => {
   return (
     <div className="fixed bottom-0 left-0 right-0 w-full border border-t border-[#ececec] bg-white">
-      <div className="flex">
-        <label className="w-75 mb-4 mt-7 flex h-11 flex-1 cursor-pointer items-center justify-center gap-4 rounded-full bg-[#ef797b] text-center text-sm font-medium text-white">
-          <input
-            className="hidden"
-            type="file"
-            name="file"
-            onChange={onChange}
-          />
-          {type == 1 ? <IconPhotoFilled color="#fff" /> : null}
-          画像で食材を入力
-        </label>
+      <div className="mb-6">
+        {type === 1 ? (
+          !!onChange ? (
+            <FileButton onChange={onChange}>
+              {(props) => (
+                <Button
+                  {...props}
+                  variant="filled"
+                  fullWidth
+                  size="md"
+                  radius="xl"
+                  leftSection={<IconPhotoFilled color="#fff" />}
+                >
+                  画像で食材を入力
+                </Button>
+              )}
+            </FileButton>
+          ) : null
+        ) : (
+          <Button
+            variant="filled"
+            radius="xl"
+            size="md"
+            fullWidth
+            onClick={onClick}
+          >
+            決定する
+          </Button>
+        )}
       </div>
     </div>
   );
